b9ada38621a4711f674cca4bb78e157219bec73f
achraf <achraf.zahouani.33@gmail.com>
2025-01-16 00:13:04 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { Sonner } from 'sonner';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from '@/pages/Index';
 
 const queryClient = new QueryClient();
 
@@ -22,3 +22,4 @@ const App = () => (
 );
 
 export default App;
+
